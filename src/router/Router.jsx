eaa4e22a755b34ff2e61ba1706d896b3ce46fca7
unknown <sadikatanisha@gmail.com>
2025-10-26 16:03:01 +0600

    Initial Setup

---
+++ src/router/Router.jsx
@@ -0,0 +1,53 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import Home from "../pages/Home/page.jsx";
+import About from "../pages/About/page.jsx";
+import Contact from "../pages/Contact/page.jsx";
+import Candidates from "../pages/Candidates/page.jsx";
+import Campaigns from "../pages/Campaigns/page.jsx";
+import News from "../pages/News/page.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+      {
+        index: true,
+        path: "/candidates",
+        Component: Candidates,
+      },
+      {
+        index: true,
+        path: "/campaigns",
+        Component: Campaigns,
+      },
+      {
+        index: true,
+        path: "/news",
+        Component: News,
+      },
+      {
+        index: true,
+        path: "/campaigns",
+        Component: Campaigns,
+      },
+      {
+        path: "/contact",
+        Component: Contact,
+      },
+      {
+        path: "/about",
+        Component: About,
+      },
+    ],
+  },
+]);
+
+export default router;
