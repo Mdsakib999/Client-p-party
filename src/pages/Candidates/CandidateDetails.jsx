3526501da8f3c34034c875b58a57185476dc475e
unknown <sadikatanisha@gmail.com>
2025-11-06 14:56:05 +0600

    color fix

--- src/pages/Candidates/CandidateDetails.jsx
+++ src/pages/Candidates/CandidateDetails.jsx
@@ -55,7 +55,7 @@ export default function CandidateDetails() {
                   </span>
                 )}
                 {details.category && (
-                  <span className="bg-blue-100 text-blue-800 px-3 py-1 rounded-full text-sm font-medium">
+                  <span className="bg-blue-100 text-green-800 px-3 py-1 rounded-full text-sm font-medium">
                     {details.category}
                   </span>
                 )}
