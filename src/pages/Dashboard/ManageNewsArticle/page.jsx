9a54e0b61e83a57e050369706320a2dc8b516122
Md. Shahidul Islam <shanto57575@gmail.com>
2025-11-13 18:55:26 +0600

    news article functionality completed

--- src/pages/Dashboard/ManageNewsArticle/page.jsx
+++ src/pages/Dashboard/ManageNewsArticle/page.jsx
@@ -2,7 +2,7 @@ import BlogTable from "../../../components/Campaign/news/BlogTable";
 
 export default function ManageNewsArticle() {
   return (
-    <div className="p-6">
+    <div className="p-10">
       <h2 className="text-2xl font-bold mb-6">Manage News Articles</h2>
       <BlogTable />
     </div>
