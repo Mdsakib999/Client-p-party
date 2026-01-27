082c474566fe09ca26121e871ddacfe14a2c866f
Md. Shahidul Islam <shanto57575@gmail.com>
2026-01-27 06:11:54 +0600

    added photo frame with candidate details page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { baseApi } from "./baseApi";
-import { frameEditorApi } from "./features/frameEditor/frameEditor.api";
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    [frameEditorApi.reducerPath]: frameEditorApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      baseApi.middleware,
-      frameEditorApi.middleware,
-    ),
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 setupListeners(store.dispatch);
